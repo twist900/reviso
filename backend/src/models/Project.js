f95af9cb39502693df6a77bfd319d7960ac1196b
Georgy Shabunin <georgy.shabunin@gmail.com>
2018-02-17 22:24:50 +0100

    Add fields to Project model

--- backend/src/models/Project.js
+++ backend/src/models/Project.js
@@ -4,7 +4,9 @@ import uniqueValidator from 'mongoose-unique-validator';
 const Schema = mongoose.Schema;
 const schema = new Schema({
   name: { type: String, required: true, unique: true },
-  client: { type: String, required: true }
+  client: { type: String, required: true },
+  description: { type: String },
+  timeTotal: { type: Number, default: 0 } // cached value based on recordings
 });
 
 schema.plugin(uniqueValidator);
